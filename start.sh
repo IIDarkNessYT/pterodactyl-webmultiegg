82d2316118521a40228d9a5c8d33c0a18e475d54
_DarkNessYT <darknessyt@inbox.ru>
2024-05-10 11:03:26 +0300

    For Beta 3

--- start.sh
+++ start.sh
@@ -186,7 +186,7 @@ else
         ./sbin/nginx -p '/home/container/nginx/' -g 'daemon off;'
     elif [ "$SERVER_TYPE" = "nginx with php-fpm" ]; then
         echo -en "\033[1;33mWebMultiEgg: \033[22;37mNginx и PHP-FPM были успешно запущены. Все логи Nginx находятся в папке /logs.\n\033[1;33mWebMultiEgg: \033[1;31mВНИМАНИЕ! \033[22;37mНе удаляйте папку .eggSystem, поскольку эта папка хранит данные о вашем сервере и она является ядром для этого сервера.\n\033[1;33mWebMultiEgg: \033[22;37m Файлы конфигурации для OpenResty находятся в папке /conf.\nㅤ"
-        ./sbin/php-fpm -c /home/container/etc/php.ini --daemonize
+        ./sbin/php-fpm -c /home/container/etc/php.ini
         ./sbin/nginx -p '/home/container/nginx/' -g 'daemon off;'
     fi
 fi
