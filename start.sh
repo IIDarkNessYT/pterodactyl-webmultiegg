b3cbaf95e142cd8e83e8a24d018e1416d3f8f81f
_DarkNessYT <darknessyt@inbox.ru>
2024-05-10 16:30:09 +0300

    TEST

--- start.sh
+++ start.sh
@@ -187,7 +187,8 @@ else
         ./sbin/nginx -p '/home/container/nginx/' -g 'daemon off;'
     elif [ "$SERVER_TYPE" = "nginx with php-fpm" ]; then
         echo -en "\033[1;33mWebMultiEgg: \033[22;37mNginx и PHP-FPM были успешно запущены. Все логи Nginx находятся в папке /logs.\n\033[1;33mWebMultiEgg: \033[1;31mВНИМАНИЕ! \033[22;37mНе удаляйте папку .eggSystem, поскольку эта папка хранит данные о вашем сервере и она является ядром для этого сервера.\n\033[1;33mWebMultiEgg: \033[22;37m Файлы конфигурации для Nginx находятся в папке /conf.\nㅤ"
-        ./sbin/php-fpm -c /home/container/etc/php.ini --daemonize -m | grep curl
+        echo "$(locate easy.h)"
+        ./sbin/php-fpm -c /home/container/etc/php.ini --daemonize
         ./sbin/nginx -p '/home/container/nginx/' -g 'daemon off;'
     fi
 fi
