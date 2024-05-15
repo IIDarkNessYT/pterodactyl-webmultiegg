0dd59d327a3c22bd5145e69194e4fe7fa2d19ed3
_DarkNessYT <darknessyt@inbox.ru>
2024-05-15 23:02:57 +0300

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -9,5 +9,6 @@ cd /home/container/.eggSystem
 wget -O start.sh https://raw.githubusercontent.com/iidarknessyt/pterodactyl-webmultiegg/main/start.sh -q -o /dev/null && chmod 755 start.sh
 cd /home/container
 echo -en "\n\033[1;33mWebMultiEgg: \033[22;32mФайлы запуска были успешно скачаны.\033[0m\n"
+chmod -R 777 /home/container/
 sleep 5
 eval ./.eggSystem/start.sh
